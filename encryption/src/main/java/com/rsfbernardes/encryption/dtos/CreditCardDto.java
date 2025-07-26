e47e07578b300a761860bb9aa3286039f3890c16
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2025-07-26 13:09:30 -0300

    Create dto

---
+++ encryption/src/main/java/com/rsfbernardes/encryption/dtos/CreditCardDto.java
@@ -0,0 +1,12 @@
+package com.rsfbernardes.encryption.dtos;
+
+import java.io.Serializable;
+
+public class CreditCardDto implements Serializable {
+
+    Long id;
+    String userDocument;
+    String creditCardToken;
+    Long value;
+
+}
